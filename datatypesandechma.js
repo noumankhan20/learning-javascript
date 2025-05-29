d30868eae237c9dcbe8c24d96c0f7030c290ed7f
noumankhan20 <noumankhan8097@gmail.com>
2025-05-29 17:08:11 +0530

    add initial JavaScript file for data types and schema examples

---
+++ datatypesandechma.js
@@ -0,0 +1,15 @@
+let name ="abujojo";
+let age= 19;
+let isLoggedIn = false;
+console.log([name ,age,isLoggedIn]);
+
+//datatypes in js are 
+// Number
+// String = "hello world"
+// Boolean = true/false
+// Null = null standalone value
+// undefined = variable is declared but not assigned any value
+// symbol = unique and immutable value
+console.log(typeof age);
+console.log(typeof undefined);// iska output undefined aayega
+console.log(typeof null);// iska output object aayega 

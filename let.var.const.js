f778c4881abb67d43042f7952994a9dec7fd1c1b
noumankhan20 <noumankhan8097@gmail.com>
2025-05-29 16:30:37 +0530

    just the basics

--- let.var.const.js
+++ let.var.const.js
@@ -1,1 +1,22 @@
 // hello this i started for learning the javascript
+const name = "Nouman";
+var email = "abujojo@example.com";
+let dob="2006";
+// name ="Nouman";
+console.log(name);
+console.log(email);
+// let and const are mostly used in modern JavaScript ignore var we should prefer let and const
+age="21";
+console.log(age);
+dob="1999";
+console.log(dob);
+let phonenumber;//agr koi variable declare nahi kiya to undefined aata hai
+console.table([name,email,dob,age,phonenumber])//used to print in table format
+
+
+
+
+
+
+
+

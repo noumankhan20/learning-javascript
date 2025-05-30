588ccbf0dd6816ff56d3311e618600b9b4ca9499
noumankhan20 <noumankhan8097@gmail.com>
2025-05-30 11:25:03 +0530

    add JavaScript examples for string and comparison operators

---
+++ conversion.js
@@ -0,0 +1,36 @@
+// //this is just a simple conversion which everyone would know the output of
+// let value=4;
+// let negvalue= -value;
+// console.log(negvalue);// as you know the output of this is -4
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // exponentiation operator matlab 2 raised to the power of 2
+// console.log(2/2);// division operator matlab 2 divided by 2(output is 1)
+// console.log(2%2); // modulus operator matlab remainder of 2 divided by 2(remainder is 0)
+ 
+//the above ones are the arithmetic operators
+//here are some string operators
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2; // string concatenation operator
+console.log(str3); // output is "Hello World"
+
+
+//now as we finished the string operators and the arithmetic operators 
+//let's move on to the comparison operators
+console.log(2+'2'); // this will output "22" because the number 2 is converted to a string and concatenated with the string '2'
+console.log(2+2+"3"); // this will output "43" because the first 2 is added to the second 2 and then the result is concatenated with the string "3"
+console.log("2"+"2"+"2"); // this will output "222" because all the strings are concatenated together
+// if you want to know more about the comparison operators, you can check out the MDN documentation on JavaScript operators: "https://developer.mozilla.org/en-US/search?q=JavaScript" ot tc39 documentation on JavaScript operators: "https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations"
+
+
+
+let game=100;
+game++;
+console.log(game); // output is 101 because the game variable is incremented by 1
+//for more such implementations on prefix and postfix increment and decrement operators, you can check out the MDN documentation on JavaScript operators: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment"
+
+
+

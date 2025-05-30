e2fdbe80a7c690caf8d9edbdaf2177b33f94ab1c
noumankhan20 <noumankhan8097@gmail.com>
2025-05-30 17:30:01 +0530

    add comparison examples in JavaScript

---
+++ comparison.js
@@ -0,0 +1,8 @@
+// now will do comparion in js
+console.log("2">1); // this will output true because 2 is greater than 1 and it converted the string "2" to in a number 2 and then compared it 
+console.log(null == 0);// this will output false because null is not equal to 0
+console.log(null == undefined); // this will output true because null and undefined are considered equal in JavaScript
+console.log(null >=0);// this will output true because null is considered greater than or equal to 0 in JavaScript
+console.log(null <0);
+console.log(null >0); // this will output false because null is not greater than 0
+console.log(undefined == 0); // this will output false because undefined is not equal to 0

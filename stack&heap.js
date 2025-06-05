7238e2fe56286c4040f8ea504256b83f18134b78
noumankhan20 <noumankhan8097@gmail.com>
2025-06-05 15:42:55 +0530

    add initial examples for data types and memory allocation in JavaScript

---
+++ stack&heap.js
@@ -0,0 +1,3 @@
+// if a memory is defined in the stack it is said to be primitive function and if one is defined in the heap 
+// it is said to be in non-primitive 
+let username = "should be like that";

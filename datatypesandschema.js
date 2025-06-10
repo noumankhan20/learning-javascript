a2416bd4ea1f5ed52f348399f8ddad5a01fa0813
noumankhan20 <noumankhan8097@gmail.com>
2025-06-10 16:30:38 +0530

    add examples for stack and heap memory in JavaScript

--- datatypesandschema.js
+++ datatypesandschema.js
@@ -41,3 +41,27 @@ console.log(typeof null);// iska output object aayega
 
 // stack is the one which store the primitive values in js like number, string, boolean, null, undefined, symbol
 // heap is the one which store the non-primitive values in js like objects, arrays, functions, etc
+
+// Lets start with the stack memory 
+let nameoftheguy= "Nouman"
+let anothername= nameoftheguy;
+anothername = "Abujojo";
+console.log(anothername);
+ console.log(nameoftheguy);// as in see here simple hai jaise maine anothername ko "Abujojo" assign kiya tha 
+ // to wo sirf anothername ko change karega aur nameoftheguy ko nahi karega kyuki ye stack memory me hai
+
+ // Now lets see the heap memory
+ let userOne={
+    email:"nouman@example.com",
+    name:"Nouman",
+ }
+ let userTwo = userOne;
+ userTwo.email ="abujojo@example.com";
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
+
+ //as you can see here userOne and userTwo are pointing to the same object in the heap memory aur heap memory mein 
+ // reference hota h matlab jaise maine userOne ko assign kara userTwo to wo dono same jagah point karenge 
+ // and jaise maine userTwo.email ko change kara to wo userOne.email ko bhi change karega
+ // kyuki dono same jagah point kar rahe hain heap memory mein

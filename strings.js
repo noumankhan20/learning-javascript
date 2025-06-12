d12271a60797151907e79f2dfcd4a75a4240bbea
noumankhan20 <noumankhan8097@gmail.com>
2025-06-12 12:14:00 +0530

    add examples for string concatenation and methods in JavaScript as you can see here stringsin js

---
+++ strings.js
@@ -0,0 +1,15 @@
+// here we are gonna strings in javascript
+const name ="Nouman";
+const greeting="Hello";
+console.log(greeting+name + "How are you")// this is called as concatenation in javascript but this is old 
+//now we use back ticks in js 
+
+console.log(`hii ${name.toUpperCase()} hwo do you do ?${greeting}`);// this is new way of concatenation in javascript which is used mostly 
+//here in the above you can use many thingsuppercase
+
+const newName= new String("Abujojo");
+console.log(newName[1]);// as it is only easy for you to access through an index
+
+//similarly you can use many string methods in javascript like
+console.log(newName.length);// for more like this you can use many string methods in javascript
+// you can use teh console.log and see in the F12 console in the browser 

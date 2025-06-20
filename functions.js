8f12331e5937154490e163808e0c62a9a34d606d
noumankhan20 <noumankhan8097@gmail.com>
2025-06-20 11:59:52 +0530

    functions for handling objects and returning specific array values

--- functions.js
+++ functions.js
@@ -24,3 +24,37 @@ console.log(login("Nouman"));// this line will give us the output as we did cons
  login("Nouman")// this line will not give us any output 
 // login("Akshat")
 // console.log(login("akshat"));
+
+function calculation(...num1){// these 3 dots are called as rest operators and spread operators but depending on the use case 
+    //they are divided into rest and spread operators
+    return num1
+}
+console.log(calculation(200,300,400,500));// for now these 3 dots are used as rest operators and its output will be in as arrays 
+// now will have another option over here 
+
+function calculated(val1,val2, ...num1){
+      return num1
+}
+console.log(calculated(200,500,600,1000));// these is where the val1 and val2 will have the values of the 200=val1 and 500=val2 
+// 
+const user={
+    username:"Nouman",
+    age:19
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and age is ${anyObject.age}`);
+    
+}// this is where we can access the object using the function 
+handleObject(user)
+handleObject({
+    username:"Abujojo",
+    age:20
+})
+
+const myNewArray=[200,300,500,1000]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+console.log(returnSecondValue(myNewArray));

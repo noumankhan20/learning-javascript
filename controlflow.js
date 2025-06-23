ee67fce3f4dbc8178e884c95f86f674b6b76fb50
noumankhan20 <noumankhan8097@gmail.com>
2025-06-23 14:54:40 +0530

    Added  initial control flow and execution context explanations in separate files

---
+++ controlflow.js
@@ -0,0 +1,13 @@
+// Here will be going through our control flow statements 
+// one of it is if-statements.
+// before moving ahead will just have a look at here meaning that (=) is used to check the
+// as will just have a basic look at the operators in the js which are everywhere mostly same 
+// < it is called as less than operator
+// > it is called as greater than operator
+// <= it is called as less than equal to operator
+// >= it is called as greater than equal to operator
+// = this single equal is operator assignment it is used to assign the value 
+// == this double equal operator is used check that is the value is equals to that comapred value eg. 4==4
+// === now here comes the triple equal to operator it used to strictly check that if value is striclty equals to the another value
+// != this is called as not equal to operator 
+
---
+++ jec.js
@@ -0,0 +1,3 @@
+// JEC means Javascript Execution context 
+// in this there are 2 main contexts (Global execution context and function execution context)
+// will have another overview about it while preparing for the interview

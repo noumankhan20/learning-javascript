233fdac6cec9aa8286a3a3ca50320d21ef6352a5
noumankhan20 <noumankhan8097@gmail.com>
2025-09-27 22:35:53 +0530

    bored

--- controlflow.js
+++ controlflow.js
@@ -1,5 +1,4 @@
 // Here will be going through our control flow statements 
-// one of it is if-statements.
 // before moving ahead will just have a look at here meaning that (=) is used to check the
 // as will just have a basic look at the operators in the js which are everywhere mostly same 
 // < it is called as less than operator
@@ -10,4 +9,23 @@
 // == this double equal operator is used check that is the value is equals to that comapred value eg. 4==4
 // === now here comes the triple equal to operator it used to strictly check that if value is striclty equals to the another value
 // != this is called as not equal to operator 
+// (just a note is there that var kekyword which is there is having the whole scope as global)
+// one of it is if-statements.
+const number=80
+if(number===80){
+    console.log(" Number is less than 100 ");
+    
+}
+else{
+    console.log("Number is greater than 100")
+}
+console.log("Executed")
 
+
+const score= 200
+if (score>100){
+    const power ="fly"
+    console.log(`Use power: ${power}`);
+}
+// console.log(`User power:${power}`)// as we can see overe here that we cannot access the function outside the block scope 
+// as like this we can use many if else consditions statements and else-if statements too. 
